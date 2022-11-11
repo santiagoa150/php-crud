500127abd91c5849c2395583592c1378c6860294
santiagoa150 <santiago.alvarez@innventasystem.com>
2022-11-11 00:01:43 -0500

    feat: ajax

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
-const getOne = (id) => {
-    $.ajax({
-        method: 'POST',
-        url: 'get-one.php',
-        data: {
-            id,
-        }
-    }).done((response) => {
-        return response;
-    })
-}
-
 const setListeners = () => {
     const elements = document.getElementsByClassName('inventory-button');
     for (const element of elements) {
         element.addEventListener('click', (e) => {
             e.preventDefault();
             const name = element.getAttribute('name');
-            if (name.substring(0, 6) === 'detail'){
+            if (name.substring(0, 6) === 'detail') {
                 const modal = document.getElementById('product-detail');
                 const id = name.substring(7);
-                const htmlElement = getOne(id);
-                if (htmlElement !== ''){
-                    modal.innerHTML = htmlElement;
-                    modal.style.display = 'flex';
-                }
+                $.ajax({
+                    method: 'POST',
+                    url: 'get-one.php',
+                    data: {
+                        id,
+                    }
+                }).done((response) => {
+                    if (response && response !== '') {
+                        modal.innerHTML = response;
+                        modal.style.display = 'flex';
+                    }
+                })
             }
         });
     }
